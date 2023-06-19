427c8b4bf0ef8d452159719f1684a64986e26da9
BambooCode98 <jw.thomas98@yahoo.com>
2023-06-19 12:48:09 -0500

    changed star timing

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ class Star{
   strobe() {
     
     // console.log(this.inc);
-    this.inc+=0.01*this.sparkle;
+    this.inc+=0.05*this.sparkle;
     ctx.shadowBlur = this.blur + Math.sin(this.inc)*2;
     // this.inc += 1;
   }
